362dc1f53e5731b4799fa960e765f2fd9e26e674
Ruzbihan <ruzbihanmzm@gmail.com>
2024-10-31 13:13:47 +0530

    Nav bar implementation

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,15 @@
+"use client";
+
+import Link from "next/link";
+
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
-  )
-}
+    <nav className="flex justify-between p-4 bg-gray-800 text-white">
+      <Link href="/">Home</Link>
+      <Link href="/submit">Submit Joke</Link>
+      <Link href="/jokes">View Jokes</Link>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
