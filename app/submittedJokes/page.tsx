d199dc0a74edb7209701ee0f437bd5561d4472dd
Ruzbihan <ruzbihanmzm@gmail.com>
2024-10-31 18:46:13 +0530

    protected routing for submitted jokes list

--- app/submittedJokes/page.tsx
+++ app/submittedJokes/page.tsx
@@ -1,6 +1,19 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import SubmittedJokeList from "../components/submittedJokeComponent/SubmittedJokeList";
+import { useEffect } from "react";
 
 const SubmittedJokesPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      router.push("/");
+    }
+  }, [router]);
+
   return (
     <div className="flex flex-col items-center space-y-4">
       <SubmittedJokeList />
