1801edb039ce3b0802c2e4356f3d484fc8c4c8f2
Ruzbihan <ruzbihanmzm@gmail.com>
2024-10-31 17:02:40 +0530

    nav bar changes

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,13 +1,35 @@
 "use client";
 
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
+  const [isAuthorized, setIsAuthorized] = useState(false);
+
+  useEffect(() => {
+    // Check if the user is authorized by looking for a token in local storage
+    const token = localStorage.getItem("token");
+    if (token) {
+      setIsAuthorized(true);
+    } else {
+      setIsAuthorized(false);
+    }
+  }, []);
+
   return (
     <nav className="flex justify-between p-4 bg-gray-800 text-white">
-      <Link href="/">Home</Link>
-      <Link href="/submit">Submit Joke</Link>
-      <Link href="/jokes">View Jokes</Link>
+      <div className="flex space-x-4">
+        <Link href="/">Home</Link>
+        <Link href="/submit">Submit Joke</Link>
+        <Link href="/jokes">View Jokes</Link>
+      </div>
+      
+      {/* Conditionally render the Unmoderate Jokes link */}
+      {isAuthorized && (
+        <div className="ml-4">
+          <Link href="/submittedJokes">Submitted Jokes</Link>
+        </div>
+      )}
     </nav>
   );
 };
