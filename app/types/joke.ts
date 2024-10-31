7b0ea56c85a21512157260e84d158afde627d2c4
Ruzbihan <ruzbihanmzm@gmail.com>
2024-10-31 16:53:08 +0530

    joke type modified to handle two types of Jokes

--- app/types/joke.ts
+++ app/types/joke.ts
@@ -1,5 +1,15 @@
-export interface Joke {
-  id: string;
+interface BaseJoke {
   content: string;
   type: string;
+}
+
+// SQL Joke interface extending BaseJoke
+export interface DeliveredJoke extends BaseJoke {
+  id: string;
+}
+
+// MongoDB Joke interface extending BaseJoke
+export interface SubmittedJoke extends BaseJoke {
+  _id: string;
+  isDelivered: boolean;
 }
