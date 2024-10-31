eebfb6480354d27405e03a3a74983b356dd733dc
Ruzbihan <ruzbihanmzm@gmail.com>
2024-10-31 13:13:55 +0530

    constants

--- app/constants/apiConstants.ts
+++ app/constants/apiConstants.ts
@@ -1,8 +1,36 @@
-const API_BASE_URL = "http://localhost:3001/moderateApi";
+const MODERATE_API_BASE_URL = "http://localhost:3001/moderateApi";
 
-const API_ENDPOINTS = {
-  LOGIN: `${API_BASE_URL}/auth/login`,
-  JOKES: `${API_BASE_URL}/jokes`,
+const MODERATE_API_ENDPOINTS = {
+  LOGIN: `${MODERATE_API_BASE_URL}/auth/login`,
+  JOKES: `${MODERATE_API_BASE_URL}/jokes`,
 };
 
-export { API_BASE_URL, API_ENDPOINTS };
+const SUBMIT_API_BASE_URL = "http://localhost:3000/jokes";
+
+const SUBMIT_API_ENDPOINTS = {
+  JOKES_TYPES: `${SUBMIT_API_BASE_URL}/types`,
+};
+
+export const JOKE_TYPES = [
+  "Pun",
+  "One-liner",
+  "Knock-Knock",
+  "Dad Joke",
+  "Dark Humor",
+  "Political",
+  "Animals",
+  "Programming",
+  "Wordplay",
+  "Observational",
+  "Nerdy",
+  "Tech",
+  "Science",
+  "Food",
+];
+
+export {
+  MODERATE_API_BASE_URL,
+  MODERATE_API_ENDPOINTS,
+  SUBMIT_API_BASE_URL,
+  SUBMIT_API_ENDPOINTS,
+};
